183b195b2ff4605130fa06400eab41fc77091464
David Renard <elscrappo@Davids-MBP-2.attlocal.net>
2020-03-09 15:07:54 -0400

    complete page

---
+++ lab4.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+
+    echo "<h2>Your personal information:</h2>";
+    echo "Gender: " . $_POST["gender"]. "<br>";
+     echo "Age: " . $_POST["age"]. "<br>";
+      echo "Level: " . $_POST["level"]. "<br>";
+      echo "School: " . $_POST["school"]. "<br>";
+      echo "Choose between 1 - 100: " . $_POST["score"]. "<br>";
+      echo "Have your computer been affected by a virus: " . $_POST["virus"]. "<br>";
+      echo "What Antivirus software do you usually use: " . $_POST["Norton"]. "<br>";
+
+      echo "I know how to creat a strong password: " . $_POST["evening"]. "<br>";
+      echo "I know how to remove virus from my computer: " . $_POST["morning"]. "<br>";
+      echo "I know how to remove sypyware from my computer: " . $_POST["night"]. "<br>";
+      echo "I know how to secure my wireless connection at home: " . $_POST["afternoon"]. "<br>";
+      echo "I know how to protect my personal informtion while searching the Internet: " . $_POST["yesterday"]. "<br>";
+
+
+
+
+?>
